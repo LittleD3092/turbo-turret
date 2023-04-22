0c8d3304b3ea58d544684cf9ec7587d5dacf4010
littled3092 <drevild01801@gmail.com>
2023-04-22 20:19:07 +0800

    fix bug

--- src/turret/turret/turret.py
+++ src/turret/turret/turret.py
@@ -12,6 +12,10 @@ def printByMe(printMsg = ''):
 import serial
 try:    
     ser = serial.Serial('/dev/ttyACM0', 9600)
+except:
+    print('Serial port 0 is unavailable.')
+try:
+    ser = serial.Serial('/dev/ttyACM1', 9600)
 except: 
     printByMe('Arduino is not connected.')
     exit()
