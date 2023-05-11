9ebebab35036f099207ff01b1501b44ac8aed83e
littled3092 <drevild01801@gmail.com>
2023-05-11 10:08:24 +0800

    test reloading mechanism

--- src/turret/script/arduino_side/arduino_side.ino
+++ src/turret/script/arduino_side/arduino_side.ino
@@ -11,9 +11,22 @@
 #define FIRING_STEP_INTERFACE 1
 #define FIRING_STEP_PER_REV 1600
 
+#define GENEVA_STEP_DIR 7 // CW+
+#define GENEVA_STEP_PUL 9 // CLK+
+#define GENEVA_STEP_INTERFACE 1
+#define GENEVA_STEP_PER_REV 1600
+
+#define PISTON_STEP_DIR 11 // CW+
+#define PISTON_STEP_PUL 13 // CLK+
+#define PISTON_STEP_INTERFACE 1
+#define PISTON_STEP_PER_REV 1600
+#define PISTON_STEP_MAX_SPEED 1000
+#define PISTON_STEP_ACCELERATION 500
+
 // stepper motor
 AccelStepper yawStep(YAW_STEP_INTERFACE, YAW_STEP_PUL, YAW_STEP_DIR);
 AccelStepper firingStep(FIRING_STEP_INTERFACE, FIRING_STEP_PUL, FIRING_STEP_DIR);
+AccelStepper genevaStep(GENEVA_STEP_INTERFACE, GENEVA_STEP_PUL, GENEVA_STEP_DIR);
 
 // servo
 Servo rightServo;
@@ -32,6 +45,35 @@ Servo leftServo;
 
 #define RIGHT_SERVO_INCREMENT -5
 #define LEFT_SERVO_INCREMENT 5
+
+// global variables
+int remainingAmmo = 3;
+
+// classes
+class Piston
+{
+public:
+    Piston(int interface, int pul, int dir, int perRev, int maxSpeed, int acceleration)
+    {
+        stepper = AccelStepper(interface, pul, dir);
+        stepper.setMaxSpeed(maxSpeed);
+        stepper.setAcceleration(acceleration);
+    }
+    void moveForward()
+    {
+        stepper.move(PISTON_STEP_PER_REV * 0.25);
+        stepper.runToPosition();
+    }
+    void moveBackward()
+    {
+        stepper.move(PISTON_STEP_PER_REV * -0.25);
+        stepper.runToPosition();
+    }
+private:
+    AccelStepper stepper;
+};
+
+Piston piston(PISTON_STEP_INTERFACE, PISTON_STEP_PUL, PISTON_STEP_DIR, PISTON_STEP_PER_REV, PISTON_STEP_MAX_SPEED, PISTON_STEP_ACCELERATION);
 
 // check whether x is between min and max (inclusive)
 // Precondition: x is the value to check, min and max are the bounds
@@ -54,6 +96,8 @@ void setup()
     yawStep.setAcceleration(500);
     firingStep.setMaxSpeed(4000);
     firingStep.setAcceleration(4000);
+    genevaStep.setMaxSpeed(1000);
+    genevaStep.setAcceleration(500);
     Serial.write("Stepper motor initialized\n");
 
     rightServo.attach(RIGHT_SERVO_PIN);
@@ -163,13 +207,66 @@ void loop()
         Serial.println("servo lowered");
     }
 
-    if(input == "fire")
+    if(input == "fire" && remainingAmmo > 0)
     {
+        remainingAmmo--;
+
+        // piston forward
+        piston.moveForward();
+        // fire
         firingStep.move(FIRING_STEP_PER_REV * 0.5);
         firingStep.runToPosition();
         firingStep.move(-FIRING_STEP_PER_REV * 0.55);
         firingStep.runToPosition();
+        // piston back
+        piston.moveBackward();
+
+        if(remainingAmmo != 0)
+        {
+            // reload
+            genevaStep.move(GENEVA_STEP_PER_REV * 2.4);
+            genevaStep.runToPosition();
+        }
+
         Serial.println("fired");
+    }
+    else if(input == "fire" && remainingAmmo == 0)
+    {
+        Serial.println("out of ammo");
+    }
+
+    if(input == "reset" && remainingAmmo < 3)
+    {
+        remainingAmmo++;
+
+        // reset
+        genevaStep.move(-GENEVA_STEP_PER_REV * 2.4);
+        genevaStep.runToPosition();
+
+        Serial.println("reset");
+    }
+    else if(input == "reset" && remainingAmmo == 3)
+    {
+        Serial.println("already at max ammo");
+    }
+
+    if(input == "force reset")
+    {
+        remainingAmmo++;
+        if(remainingAmmo > 3)
+            remainingAmmo = 3;
+
+        // reset
+        genevaStep.move(-GENEVA_STEP_PER_REV * 2.4);
+        genevaStep.runToPosition();
+
+        Serial.println("force reset");
+    }
+
+    if(input == "piston test")
+    {
+        piston.moveForward();
+        piston.moveBackward();
     }
 
     // clear serial buffer
